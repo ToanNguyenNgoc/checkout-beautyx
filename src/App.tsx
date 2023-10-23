6cb5fd2827246ddd1de9e5c26c9a0ada72754ed4
Nguyen Ngoc Toan <ngoctoan06011998@gmail.com>
2023-10-23 14:51:09 +0700

    xxx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import queryString from "query-string";
+import "./App.css"
 
 function App() {
   const search = window.location.search;
   const params = queryString.parse(search)
   return (
-    <div>
-     <iframe
-      src={`${params.payurl}&s=${params.s}`}
-      style={{
-        width:'100vw',
-        height:'100vh'
-      }}
-     />
+    <div className="cnt">
+      <iframe
+        className="iframe-cnt"
+        src={`${params.payurl}&s=${params.s}`}
+      />
+      <div className="trans"></div>
     </div>
   );
 }
